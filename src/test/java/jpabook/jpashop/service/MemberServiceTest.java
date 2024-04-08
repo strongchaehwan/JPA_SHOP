5a5be72e4b4962d68448462b01eb9626c2ad15a4
limchaehwan <chlim20@naver.com>
2024-04-08 18:34:23 +0900

    회원 도메인

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,55 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Rollback(false)
+    public void 회원가입() throws  Exception{
+        //Given
+        Member member = new Member();
+        member.setName("임채환");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        assertEquals(member,memberRepository.findOne(saveId));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception{
+        //given
+        Member member1 = new Member();
+        member1.setName("lim");
+
+        Member member2 = new Member();
+        member2.setName("lim");
+
+        //when
+        memberService.join(member1);
+        memberService.join(member2);
+
+        //then
+        fail("예외가 발생해야한다..");
+
+
+    }
+}
